9b380ad6c134c0db6c918264a5b969296364cf5f
zakur <walter.barra@outlook.com>
2022-08-24 20:34:11 -0400

    [ADD] first commit

---
+++ src/test/java/ar/com/educacionit/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.educacionit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
