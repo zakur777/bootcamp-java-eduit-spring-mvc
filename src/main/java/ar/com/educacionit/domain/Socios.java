007fcdd0da55d634971fee16c702425dc8a86b39
zakur <walter.barra@outlook.com>
2022-09-05 19:08:37 -0400

    [ADD] entity+reposiroty+service+controller+template

--- src/main/java/ar/com/educacionit/domain/Socios.java
+++ src/main/java/ar/com/educacionit/domain/Socios.java
@@ -1,16 +1,39 @@
 package ar.com.educacionit.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/*
+ * MAPEO DE ENTIDADES CON HIBERNATE
+ */
+/*
+ * 
+ JPA: java persistence api (muchas interfaces)
+ Hibernate: implementacion de JPA
+ @Entity > es una entidad: representa tabla: DEBEN tener un id=PK
+ @Table: indicar el nombre de la tabla
+*/
 @Entity
 @Table(name = "socios")
+//@Data
+//@AllArgsConstructor
 public class Socios {
 
     @Id
+	//la clave en la db: es AUTOINCREMENT
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 50)
+	//pk compusta (vemos mas adelante)
+	
+	@Column(nullable = false,length = 50, name="apellido")
     private String apellido;
 
     @Column(nullable = false, length = 60)
@@ -18,4 +41,37 @@ public class Socios {
 
     @Column(nullable = false, length = 6, unique = true)
     private String codigo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
 }
--- src/main/java/ar/com/educacionit/repository/SociosRepository.java
+++ src/main/java/ar/com/educacionit/repository/SociosRepository.java
@@ -1,9 +1,15 @@
 package ar.com.educacionit.repository;
 
-import ar.com.educacionit.domain.Socios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import ar.com.educacionit.domain.Socios;
+
 @Repository
-public interface SociosRepository extends JpaRepository<Socios, Long> {
+/*
+ * T es la entidad sobre la cual voy a trabajar(Entity)
+ * ID: es el tipo de datos de la id del Entity
+ */
+public interface SociosRepository extends JpaRepository<Socios, Long>{ //EX DAO
+	//ctrl+o+o
 }
